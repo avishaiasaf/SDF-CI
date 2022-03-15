498dc8da345d634f73a58de7cc52ef7067504aa3
Avishai Asaf <avishaiasaf@Avishais-MacBook-Pro.local>
2022-03-15 16:42:03 +0200

    initial

---
+++ src/FileCabinet/SuiteScripts/UserEventScript.js
@@ -0,0 +1,29 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/record'],
+    /**
+ * @param{record} record
+ */
+    (record) => {
+
+        const EntryPoints = {};
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         * @param {Object} context
+         * @param {Record} context.newRecord - New record
+         * @param {string} context.type - Trigger type; use values from the context.UserEventType enum
+         * @param {Form} context.form - Current form
+         * @param {ServletRequest} context.request - HTTP request information sent from the browser for a client action only.
+         * @since 2015.2
+         */
+        EntryPoints.beforeLoad = (context) => {
+            log.debug('Context', context);
+        }
+
+        return {
+            ...EntryPoints
+        }
+
+    });
